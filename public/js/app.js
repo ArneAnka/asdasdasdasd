6130a5e6c6fec130531209896c0db7edfbcf6094
Johan N <info@superdb.cc>
2020-12-16 17:38:50 +0100

    css

--- public/js/app.js
+++ public/js/app.js
@@ -21304,8 +21304,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/johannilsson/code/asdasdasdasd/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/johannilsson/code/asdasdasdasd/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/johannilsson/code/asdasdasdasd2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/johannilsson/code/asdasdasdasd2/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
