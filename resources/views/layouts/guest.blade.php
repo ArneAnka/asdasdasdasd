e43a82297a4fb953121ef8bafac6f9cebd1b67a8
Johan N <info@superdb.cc>
2020-12-16 19:38:01 +0100

    css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}?version=1">
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
@@ -23,11 +23,7 @@
 
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
-            @isset($slot)
-                {{ $slot }}
-            @endisset
-        </div>
+            {{ $slot }}
         @livewireScripts
     </body>
 </html>
